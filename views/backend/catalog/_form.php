78436a565618a8da17b280daba8f726effcc5621
tangbq <tangbq@tiens.com>
2019-08-27 17:23:54 +0800

    ii

--- views/backend/catalog/_form.php
+++ views/backend/catalog/_form.php
@@ -1,10 +1,10 @@
 <?php
 
+use funson86\blog\models\BlogCatalog;
+use funson86\blog\Module;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
-use \funson86\blog\models\BlogCatalog;
-use funson86\blog\Module;
 
 /* @var $this yii\web\View */
 /* @var $model funson86\blog\models\BlogCatalog */
@@ -19,38 +19,38 @@ unset($parentCatalog[$model->id]);
 <div class="blog-catalog-form">
 
     <?php $form = ActiveForm::begin([
-        'options'=>['class' => 'form-horizontal', 'enctype'=>'multipart/form-data'],
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-2 control-label'],
-        ],
-    ]); ?>
+    'options' => ['class' => 'form-horizontal', 'enctype' => 'multipart/form-data'],
+    'fieldConfig' => [
+        'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
+        'labelOptions' => ['class' => 'col-lg-2 control-label'],
+    ],
+]);?>
 
-    <?= $form->field($model, 'parent_id')->dropDownList($parentCatalog) ?>
+    <?=$form->field($model, 'parent_id')->dropDownList($parentCatalog);?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => 255]) ?>
+    <?=$form->field($model, 'title')->textInput(['maxlength' => 255]);?>
 
-    <?= $form->field($model, 'surname')->textInput(['maxlength' => 128]) ?>
+    <?=$form->field($model, 'surname')->textInput(['maxlength' => 128]);?>
 
-    <?= $form->field($model, 'banner')->fileInput() ?>
+    <?=$form->field($model, 'banner')->fileInput();?>
 
-    <?= $form->field($model, 'is_nav')->dropDownList(BlogCatalog::getArrayIsNav()) ?>
+    <?=$form->field($model, 'is_nav')->dropDownList(BlogCatalog::getArrayIsNav());?>
 
-    <?= $form->field($model, 'sort_order')->textInput() ?>
+    <?=$form->field($model, 'sort_order')->textInput();?>
 
-    <?= $form->field($model, 'page_size')->textInput() ?>
+    <?=$form->field($model, 'page_size')->textInput();?>
 
-    <?= $form->field($model, 'template')->textInput(['maxlength' => 255]) ?>
+    <?=$form->field($model, 'template')->textInput(['maxlength' => 255]);?>
 
-    <?= $form->field($model, 'redirect_url')->textInput(['maxlength' => 255]) ?>
+    <?=$form->field($model, 'redirect_url')->textInput(['maxlength' => 255]);?>
 
-    <?= $form->field($model, 'status')->dropDownList(\funson86\blog\models\Status::labels()) ?>
+    <?=$form->field($model, 'status')->dropDownList(\funson86\blog\models\Status::labels());?>
 
     <div class="form-group">
         <label class="col-lg-2 control-label" for="">&nbsp;</label>
-        <?= Html::submitButton($model->isNewRecord ? Module::t('blog', 'Create') : Module::t('blog', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?=Html::submitButton($model->isNewRecord ? Module::t('blog', 'Create') : Module::t('blog', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end();?>
 
 </div>
--- views/backend/post/_form.php
+++ views/backend/post/_form.php
@@ -1,56 +1,60 @@
 <?php
 
-use yii\helpers\Html;
-use funson86\blog\Module;
-use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
 use funson86\blog\models\BlogCatalog;
-use kartik\markdown\MarkdownEditor;
+use funson86\blog\Module;
 use mihaildev\ckeditor\CKEditor;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model backend\modules\blog\models\BlogPost */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
+<style type="text/css">
+.col-lg-3 {
+    width: 80%;
+}
+</style>
 <div class="blog-post-form">
 
     <?php $form = ActiveForm::begin([
-        'options'=>['class' => 'form-horizontal', 'enctype'=>'multipart/form-data'],
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-2 control-label'],
-        ],
-    ]); ?>
+    'options' => ['class' => 'form-horizontal', 'enctype' => 'multipart/form-data'],
+    'fieldConfig' => [
+        'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
+        'labelOptions' => ['class' => 'col-lg-2 control-label'],
+    ],
+]);?>
 
-    <?= $form->field($model, 'catalog_id')->dropDownList(ArrayHelper::map(BlogCatalog::get(0, BlogCatalog::find()->all()), 'id', 'str_label')) ?>
+    <?=$form->field($model, 'catalog_id')->dropDownList(ArrayHelper::map(BlogCatalog::get(0, BlogCatalog::find()->all()), 'id', 'str_label'));?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => 128]) ?>
+    <?=$form->field($model, 'title')->textInput(['maxlength' => 128]);?>
 
-    <?= $form->field($model, 'brief')->textarea(['rows' => 6]) ?>
+    <?=$form->field($model, 'brief')->textarea(['rows' => 6]);?>
 
-    <?= $form->field($model, 'content')->widget(CKEditor::className(),[
-        'editorOptions' => [
-            'preset' => 'full',
-            'inline' => false,
-        ],
-    ]); ?>
+    <?=$form->field($model, 'content')->widget(CKEditor::className(), [
+    'editorOptions' => [
+        'preset' => 'full',
+        'inline' => false,
+    ],
+]);?>
 
-    <?= $form->field($model, 'tags')->textInput(['maxlength' => 128]) ?>
+    <?=$form->field($model, 'tags')->textInput(['maxlength' => 128]);?>
 
-    <?= $form->field($model, 'surname')->textInput(['maxlength' => 128]) ?>
+    <?=$form->field($model, 'surname')->textInput(['maxlength' => 128]);?>
 
-    <?= $form->field($model, 'banner')->fileInput() ?>
+    <?=$form->field($model, 'banner')->fileInput();?>
 
-    <?= $form->field($model, 'click')->textInput() ?>
+    <?=$form->field($model, 'click')->textInput();?>
 
-    <?= $form->field($model, 'status')->dropDownList(\funson86\blog\models\Status::labels()) ?>
+    <?=$form->field($model, 'status')->dropDownList(\funson86\blog\models\Status::labels());?>
 
     <div class="form-group">
         <label class="col-lg-2 control-label" for="">&nbsp;</label>
-        <?= Html::submitButton($model->isNewRecord ? Module::t('blog', 'Create') : Module::t('blog', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?=Html::submitButton($model->isNewRecord ? Module::t('blog', 'Create') : Module::t('blog', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end();?>
 
 </div>
